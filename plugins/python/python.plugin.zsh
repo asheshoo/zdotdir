019e2cab6588e9afdcedb6a2007baa12d9d7d109
Dominic Coles <dcoles@asheshoo.com>
2024-02-18 19:34:08 +1100

    Updated python plugin

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -6,20 +6,29 @@
 fpath=(${0:A:h}/functions $fpath)
 autoload -U $fpath[1]/*(.:t)
 
+# pip
+eval "$(python -m pip completion --zsh)"
+
+# pipx
+ autoload -U bashcompinit
+ bashcompinit
+ eval "$(register-python-argcomplete pipx)"
+
 # workon
 export WORKON_HOME="$XDG_DATA_HOME/venvs"
 [[ -d "$WORKON_HOME" ]] || mkdir -p "$WORKON_HOME"
 
 # python aliases
-alias py2='python2'
 alias py3='python3'
 alias py='python3'
-alias pip2update="pip2 list --outdated | cut -d ' ' -f1 | xargs -n1 pip2 install -U"
-alias pip3update="pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U"
-alias pipup="pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U"
+# alias pip3update="pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U"
+# alias pipup="python -m pip list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U"
 alias pyfind='${aliases[ff]:-find} "*.py"'
 alias pygrep='${aliases[grep]:-grep} --include="*.py"'
 alias pyva="source .venv/bin/activate"
 
 # jupyter
 export JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR:-$XDG_CONFIG_HOME/jupyter}"
+
+#pipx
+manpath+=( ${XDG_DATA_HOME}/man )
