62d036e5e484687ca4bddea79b1e4020b918089a
Dominic Coles <dcoles@asheshoo.com>
2024-04-28 18:42:40 +1000

    Updated bat plugin

--- plugins/bat/bat.plugin.zsh
+++ plugins/bat/bat.plugin.zsh
@@ -1,5 +1,8 @@
+# https://github.com/sharkdp/bat
+
 autoload-dir ${0:A:h}/functions
 
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export PAGER=bat
 alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+alias b=bat
