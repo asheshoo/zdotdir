f752e4679b8d37a9fb6818c9338f5030923726ed
Dominic Coles <dcoles@asheshoo.com>
2024-04-28 18:44:54 +1000

    Updated trunk plugin

--- plugins/trunk/trunk.plugin.zsh
+++ plugins/trunk/trunk.plugin.zsh
@@ -1,6 +1,7 @@
 #
 # trunk - Load shell extension
 #
-[[ -f "$HOME/.cache/trunk/shell-hooks/zsh.rc" ]] || return 1
+
+[[ -f "$HOME/.cache/trunk/shell-hooks/zsh.rc" ]] || return 2
 
 source $HOME/.cache/trunk/shell-hooks/zsh.rc
